1627a391879a57d8bdc181c92a127f686a5f77a9
Pedro Ribeiro de Almeida <pedroribalmeida@gmail.com>
2023-10-20 16:04:27 +1100

    Add julia script with regression model for cyclone mortality

---
+++ rrap_dg/cyclones/cyclones.jl
@@ -0,0 +1,67 @@
+using DataFrames
+using CSV
+using GLM
+
+"""
+    cyclone_mortality(filepath::String)
+
+DataFrames with windspeed and mortality data for corals (in this order):
+- Branching 3 (above depth 5)
+- Branching 3 (below depth 5)
+- Massive
+"""
+function cyclone_mortality(filepath::String)::Tuple{Function,Function,Function}
+    # Read cyclone mortality csv file
+    df = CSV.read(filepath, DataFrame; types=[Int64, Symbol, Float64, Float64])
+    df[!, :mortality] .= 0.0
+
+    # Branching 3 (above 5) mortrality
+    df_b3 = df[(df.morphology.==:branching).&(df.depth.==3), :]
+    df_b3.mortality .= max.(0 .- (df_b3.cover ./ 100), 0)
+
+    # Branching 8 (below 5) mortality
+    df_b8 = df[(df.morphology.==:branching).&(df.depth.==8), :]
+    df_b8.mortality .= max.(0 .- (df_b8.cover ./ 100), 0)
+
+    # Massive coral mortality
+    df_m = df[df.morphology.==:massive, :]
+    df_m.mortality .= 0 .- (df_m.cover ./ 100)
+
+    y_b3 = branching_regression(df_b3[!, [:windspeed, :mortality]])
+    y_b8 = branching_regression(df_b8[!, [:windspeed, :mortality]])
+    y_m = massive_regression(df_m[!, [:windspeed, :mortality]])
+
+    return y_b3, y_b8, y_m
+end
+
+logistic(x) = log((x / (1 - x)) + 0.01)
+logit2prob(x) = (exp(x) - 0.01) / (1 + exp(x) - 0.01)
+
+"""
+    branching_regression(df::DataFrame)::Function
+
+Returns prediction fuction for branching corals.
+"""
+function branching_regression(df::DataFrame)::Function
+    # Apply logistic function
+    df[!, :mortality_log] = logistic.(df.mortality)
+
+    # Linear regression
+    ols = lm(@formula(mortality_log ~ windspeed), df)
+    b, a = coef(ols)
+
+    # Convert back from logistic to prob
+    return y(x) = (exp(a * x + b) - 0.01) / (1 + exp(a * x + b) - 0.01)
+end
+
+"""
+    function massive_regression(df::DataFrame)::Function
+
+Returns prediction fuction for massive corals.
+"""
+function massive_regression(df::DataFrame)::Function
+    # Adjust linear regressioin
+    ols = lm(@formula(mortality ~ windspeed), df)
+    b, a = coef(ols)
+    return y(x) = a * x + b
+end
---
+++ rrap_dg/cyclones/cyclones.py
@@ -0,0 +1,17 @@
+import typer
+import juliacall
+
+jl = juliacall.newmodule("Cyclones")
+
+jl.seval('include("rrap_dg/cyclones/cyclones.jl")')
+
+app = typer.Typer()
+
+@app.command(help="Generate Degree Heating Week datasets")
+def generate(
+    input_path: str,
+    gen_year: str = typer.Option("2025 2100"),
+) -> None:
+    y_b3, y_b8, y_m = jl.cyclone_mortality(input_path)
+
+    print(y_b3(25))
