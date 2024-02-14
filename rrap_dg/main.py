828d49c25a2bcc9c07405130c36eb01dc2873296
ConnectedSystems <takuyai@gmail.com>
2024-02-15 10:51:15 +1100

    Add connectivity package
    Potentially rename to download

--- rrap_dg/main.py
+++ rrap_dg/main.py
@@ -2,11 +2,13 @@ import typer
 from rrap_dg import dhw
 from rrap_dg import cyclones
 from rrap_dg import cluster_domain
+from rrap_dg import connectivity
 
 app = typer.Typer()
 app.add_typer(dhw.app, name="dhw")
 app.add_typer(cyclones.app, name="cyclones")
 app.add_typer(cluster_domain.app, name="domain")
+app.add_typer(connectivity.app, name="connectivity")
 
 
 @app.callback()
