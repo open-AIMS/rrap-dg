34aaf924f78da05de363fe49a5199bc15ff5a2c2
Pedro Ribeiro de Almeida <pedroribalmeida@gmail.com>
2024-07-26 18:11:52 +1000

    Change icc file structure
    Now the downscale is only up to the species level, not size classes.

--- rrap_dg/initial_coral_cover/initial_coral_cover.py
+++ rrap_dg/initial_coral_cover/initial_coral_cover.py
@@ -7,6 +7,7 @@ jl.seval(f'include("{PKG_PATH}/initial_coral_cover/icc.jl")')
 
 app = typer.Typer()
 
+
 @app.command(help="Create new netCDF with initial coral cover values.")
 def downscale_icc(
     rrapdg_dpkg_path: str,
@@ -14,13 +15,3 @@ def downscale_icc(
     output_path: str,
 ) -> None:
     jl.downscale_icc(rrapdg_dpkg_path, target_cluster, output_path)
-
-
-@app.command(help="Create initial coral cover netCDFs with custom bin edges defined in a TOML file.")
-def bin_edge_icc(
-    rrapdg_dpkg_path: str,
-    target_cluster: str,
-    output_path: str,
-    bin_edge_file: str
-) -> None:
-    jl.downscale_icc(rrapdg_dpkg_path, target_cluster, output_path, bin_edge_file)
