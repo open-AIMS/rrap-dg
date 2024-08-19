d5cf4dd8f7f0198ec1910bf5a4f2f4835dbcbd98
Peter Baker <peter.baker122@csiro.au>
2024-08-19 15:14:26 +1000

    Uses provena python client instead of mdsisclienttools, adds README info and extensible config setup with pydantic.
    Signed-off-by: Peter Baker <peter.baker122@csiro.au>

---
+++ rrap_dg/config.py
@@ -0,0 +1,47 @@
+from provenaclient import ProvenaClient, Config
+from provenaclient.auth import DeviceFlow
+from pydantic import BaseSettings
+from typing import Optional
+
+
+class Settings(BaseSettings):
+    # The provena deployment to target
+    provena_domain: str = "mds.gbrrestoration.org"
+    # The keycloak realm name
+    provena_realm_name: str = "rrap"
+    # The keycloak client ID
+    provena_client_id: str = "automated-access"
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+def get_settings() -> Settings:
+    """
+
+    Uses the Pydantic settings management to read settings from defaults/the
+    environment.
+
+    Returns:
+        Settings: The instantiated settings object.
+    """
+    return Settings()
+
+
+def get_provena_client(settings: Optional[Settings]=None) -> ProvenaClient:
+    """
+
+    Returns an authorised provena client ready to interact with the M&DS IS.
+
+    Returns:
+        ProvenaClient: The instantiated and authenticated client.
+    """
+    _settings = settings if settings else get_settings()
+    config = Config(
+        domain=_settings.provena_domain, realm_name=_settings.provena_realm_name
+    )
+    return ProvenaClient(
+        auth=DeviceFlow(config=config, client_id=_settings.provena_client_id),
+        config=config,
+    )
--- rrap_dg/connectivity/connectivity.py
+++ rrap_dg/connectivity/connectivity.py
@@ -1,53 +1,22 @@
-from typing import Callable
-from os.path import join as pj
-from glob import glob
-
-import numpy as np
-import pandas as pd
-
-import datapackage as dpkg
-import boto3
-
-# This is a helper function for managing authentication with Provena
-import mdsisclienttools.auth.TokenManager as ProvenaAuth
-import mdsisclienttools.datastore.ReadWriteHelper as ProvenaRW
-
+from rrap_dg.config import get_provena_client
 import typer
+from asyncio import run
+
+app = typer.Typer()
 
 
-# Provena Config
-# Replace the domain with the domain of your Provena instance
-PROVENA_DOMAIN = "mds.gbrrestoration.org"
-
-app = typer.Typer() 
-  
-@app.command(help="Download connectivity matrices from M&DS datastore") 
-def download(
-    dest: str, 
-    dataset_id: str
-) -> None:
+@app.command(help="Download connectivity matrices from M&DS datastore")
+def download(dest: str, dataset_id: str) -> None:
     """Download connectivity matrices for a given cluster using the dataset id.
-       
-       Parameters
-       ----------
-       dest: str, output location of downloaded connectivity matrices
-       dataset_id: str, dataset id of the connectivity matrices
+
+    Parameters
+    ----------
+    dest: str, output location of downloaded connectivity matrices
+    dataset_id: str, dataset id of the connectivity matrices
     """
-    
-    data_store_endpoint = "https://data-api.{}".format(PROVENA_DOMAIN)
-    # Edit this to point to the Keycloak instance for your Provena instance
-    kc_endpoint = "https://auth.mds.gbrrestoration.org/auth/realms/rrap"
-
-    stage = "PROD"
-    provena_auth = ProvenaAuth.DeviceFlowManager(
-        stage=stage,
-        keycloak_endpoint=kc_endpoint,
-        auth_flow=ProvenaAuth.AuthFlow.DEVICE,
+    provena = get_provena_client()
+    run(
+        provena.datastore.io.download_all_files(
+            destination_directory=dest, dataset_id=dataset_id
+        )
     )
-
-    # expose the get auth function which is used for provena methods
-    get_auth = provena_auth.get_auth
-    ProvenaRW.download(data_store_api_endpoint=data_store_endpoint,handle=dataset_id, auth=get_auth(), download_path=dest)
-
-
-
