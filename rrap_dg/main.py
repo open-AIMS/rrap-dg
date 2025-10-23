e83a0c9716ddaa3a6ec0681fd73dce750cc50bf6
DanTanAtAims <d.tan@aims.gov.au>
2025-10-23 13:35:59 +1100

    feat: add formatting for rme_ml connectivity files for use with ADRIA

--- rrap_dg/main.py
+++ rrap_dg/main.py
@@ -1,5 +1,6 @@
 import typer
 from rrap_dg import dhw
+from rrap_dg import connectivity
 from rrap_dg import cyclones
 from rrap_dg import cluster_domain
 from rrap_dg import data_store
@@ -8,6 +9,7 @@ from rrap_dg import dpkg_template
 
 app = typer.Typer()
 app.add_typer(dhw.app, name="dhw")
+app.add_typer(connectivity.app, name="connectivity")
 app.add_typer(cyclones.app, name="cyclones")
 app.add_typer(cluster_domain.app, name="domain")
 app.add_typer(initial_coral_cover.app, name="coral-cover")
